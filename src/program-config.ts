27994e10a33a9b31ade3acbeaaafb5971c443341
Gregor <gregor.mitscha-baude@gmx.at>
2024-09-20 22:23:22 +0200

    general attestation type

--- src/program-config.ts
+++ src/program-config.ts
@@ -3,33 +3,130 @@ import {
   Bytes,
   Field,
   PublicKey,
+  Signature,
   Struct,
-  VerificationKey,
+  Undefined,
   type InferProvable,
   type ProvablePure,
 } from 'o1js';
 
+/**
+ * TODO: program spec must be serializable
+ * - can be done by defining an enum of supported base types
+ */
+
 export type { AttestationType };
 
-// dummy attestation with no proof attached
-type AttestationNone<DataType extends ProvableType> = {
-  type: 'none';
-  data: DataType;
+const Undefined_: ProvablePure<undefined> = Undefined;
+
+// TODO export from o1js
+const ProvableType = {
+  get<A extends WithProvable<any>>(type: A): ToProvable<A> {
+    return (
+      (typeof type === 'object' || typeof type === 'function') &&
+      type !== null &&
+      'provable' in type
+        ? type.provable
+        : type
+    ) as ToProvable<A>;
+  },
 };
+
+/**
+ * An attestation is:
+ * - a string fully identifying the attestation type
+ * - a type for public parameters
+ * - a type for private parameters
+ * - a type for data -- which is left generic when defining attestation types
+ * - a function `verify: (publicInput: Public, privateInput: Private, type: DataType, data: Data)` that asserts the attestation is valid
+ */
+type Attestation<
+  Id extends string,
+  PublicType extends ProvableType,
+  PrivateType extends ProvableType,
+  DataType extends ProvableType
+> = {
+  type: Id;
+  public: PublicType;
+  private: PrivateType;
+  data: DataType;
+
+  verify(
+    publicInput: InferProvableType<PublicType>,
+    privateInput: InferProvableType<PrivateType>,
+    data: InferProvableType<DataType>
+  ): void;
+};
+
+function defineAttestation<
+  Id extends string,
+  PublicType extends ProvableType,
+  PrivateType extends ProvableType
+>(config: {
+  type: Id;
+  public: PublicType;
+  private: PrivateType;
+
+  verify<DataType extends ProvableType>(
+    publicInput: InferProvableType<PublicType>,
+    privateInput: InferProvableType<PrivateType>,
+    dataType: DataType,
+    data: InferProvableType<DataType>
+  ): void;
+}): <DataType extends ProvableType>(
+  data: DataType
+) => Attestation<Id, PublicType, PrivateType, DataType> {
+  return function attestation(dataType) {
+    return {
+      type: config.type,
+      public: config.public,
+      private: config.private,
+      data: dataType,
+      verify(publicInput, privateInput, data) {
+        return config.verify(publicInput, privateInput, dataType, data);
+      },
+    };
+  };
+}
+
+// dummy attestation with no proof attached
+const ANone = defineAttestation({
+  type: 'attestation-none',
+  public: PublicKey,
+  private: Undefined_,
+  verify() {
+    // do nothing
+  },
+});
+type AttestationNone<DataType extends ProvableType> = ReturnType<
+  typeof ANone<DataType>
+>;
 
 // recursive proof
-type AttestationProof<DataType extends ProvableType> = {
-  type: 'proof';
-  data: DataType;
-  vk: VerificationKey;
-};
+const AProof = defineAttestation({
+  type: 'attestation-proof',
+  public: PublicKey,
+  private: Undefined_,
+  verify(pk, proof, type, data) {
+    throw new Error('Proof attestation not implemented');
+  },
+});
+type AttestationProof<DataType extends ProvableType> = ReturnType<
+  typeof AProof<DataType>
+>;
 
 // native signature
-type AttestationSignature<DataType extends ProvableType> = {
-  type: 'signature';
-  data: DataType;
-  issuerPubKey: PublicKey;
-};
+const ASignature = defineAttestation({
+  type: 'attestation-signature',
+  public: PublicKey,
+  private: Signature,
+  verify(issuerPk, signature, type, data) {
+    signature.verify(issuerPk, ProvableType.get(type).toFields(data));
+  },
+});
+type AttestationSignature<DataType extends ProvableType> = ReturnType<
+  typeof ASignature<DataType>
+>;
 
 type BaseData = Record<string, any>;
 
@@ -37,10 +134,6 @@ type AttestationType<DataType extends ProvableType = ProvableType> =
   | AttestationNone<DataType>
   | AttestationProof<DataType>
   | AttestationSignature<DataType>;
-
-type AttestationData<Attestation> = Attestation extends { data: infer Data }
-  ? InferProvable<Data>
-  : never;
 
 type Tuple<T> = [T, ...T[]] | [];
 
@@ -100,10 +193,8 @@ function and(left: Node<Bool>, right: Node<Bool>): Node<Bool> {
 const Bytes32 = Bytes(32);
 
 function example() {
-  let att = attestation({
-    type: 'none',
-    data: Struct({ age: Field, name: Bytes32 }),
-  });
+  let att = attestation(ASignature(Struct({ age: Field, name: Bytes32 })));
+
   let age = property(att, 'age');
   let targetAge = publicParameter(Field);
   let ageEquals = equals(age, targetAge);
@@ -116,7 +207,7 @@ function example() {
   return ageAndName;
 }
 
-console.dir(example(), { depth: null });
+console.dir(example(), { depth: 5 });
 
 // TODO these types should be in o1js
 
