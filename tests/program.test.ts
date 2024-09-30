ea835c4025502bcb9dfbf2cfe61c308f29af91ee
Marton Moro <moro.marton@gmail.com>
2024-09-30 10:52:49 +0200

    test: add run program with valid input

--- tests/program.test.ts
+++ tests/program.test.ts
@@ -1,8 +1,9 @@
 import { test } from 'node:test';
 import assert from 'node:assert';
-import { Field, Bytes } from 'o1js';
+import { Field, Bytes, PrivateKey, Signature } from 'o1js';
 import { createProgram } from '../src/program.ts';
 import { Attestation, Input, Operation, Spec } from '../src/program-config.ts';
+import { NestedProvable } from '../src/nested.ts';
 
 test('createProgram with simple spec', async (t) => {
   const Bytes32 = Bytes(32);
@@ -29,4 +30,33 @@ test('createProgram with simple spec', async (t) => {
     const vk = await program.compile();
     assert(vk, 'Verification key should be generated for zk program');
   });
+
+  await t.test('run program with valid input', async () => {
+    let data = { age: Field(18), name: Bytes32.fromString('Alice') };
+    let signedData = createAttestation(InputData, data);
+
+    const proof = await program.run({ signedData, targetAge: Field(18) });
+
+    assert(proof, 'Proof should be generated');
+
+    assert.deepStrictEqual(
+      proof.publicInput.targetAge,
+      Field(18),
+      'Public input should match'
+    );
+    assert.deepStrictEqual(
+      proof.publicOutput,
+      Field(18),
+      'Public output should match the age'
+    );
+  });
 });
+
+function createAttestation<Data>(type: NestedProvable, data: Data) {
+  const issuer = PrivateKey.random();
+  const signature = Signature.create(
+    issuer,
+    NestedProvable.get(type).toFields(data)
+  );
+  return { public: issuer.toPublicKey(), private: signature, data };
+}
