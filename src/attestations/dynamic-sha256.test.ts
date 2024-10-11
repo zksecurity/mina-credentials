8e5ee5d1f258411c51fb7ca7a6ca8ea5a29d3c7f
Gregor <gregor.mitscha-baude@gmx.at>
2024-10-11 14:02:35 +0200

    make test more interesting

--- src/attestations/dynamic-sha256.test.ts
+++ src/attestations/dynamic-sha256.test.ts
@@ -6,7 +6,7 @@ import { zip } from '../util.ts';
 
 const { SHA256 } = Gadgets;
 
-class DynamicBytes extends DynamicArray(UInt8, { maxLength: 180 }) {
+class DynamicBytes extends DynamicArray(UInt8, { maxLength: 360 }) {
   static fromString(s: string) {
     return DynamicBytes.from(
       [...new TextEncoder().encode(s)].map((t) => UInt8.from(t))
@@ -16,12 +16,14 @@ class DynamicBytes extends DynamicArray(UInt8, { maxLength: 180 }) {
 
 let bytes = DynamicBytes.fromString(longString());
 
-const StaticBytes = Bytes(longString().length);
+const StaticBytes = Bytes(stringLength(longString()));
 let staticBytes = StaticBytes.fromString(longString());
 
+let actualPadding = DynamicSHA256.padding(bytes);
+let expectedPadding = SHA256.padding(staticBytes);
 nodeAssert.deepStrictEqual(
-  DynamicSHA256.padding(bytes).toValue().map(blockToHexBytes),
-  SHA256.padding(staticBytes).map(blockToHexBytes)
+  actualPadding.toValue().map(blockToHexBytes),
+  expectedPadding.map(blockToHexBytes)
 );
 
 let actualHash = DynamicSHA256.hash(bytes);
@@ -60,6 +62,8 @@ let constraintStatic = await Provable.constraintSystem(circuitStatic);
 console.log('static', constraintStatic.summary());
 
 let ratio = constraints.rows / constraintStatic.rows;
+console.log(`static # of blocks: ${expectedPadding.length}`);
+console.log(`max # of dynamic blocks: ${actualPadding.maxLength}`);
 console.log(
   `constraint overhead for dynamic: ${((ratio - 1) * 100).toFixed(2)}%`
 );
@@ -73,8 +77,13 @@ function blockToHexBytes(block: (bigint | UInt32)[]) {
   return block.map(toHexBytes);
 }
 
-// 170 characters, needs 3 blocks
+// 342 bytes, needs 6 blocks, also contains unicode
 function longString(): string {
   return `SHA-2 (Secure Hash Algorithm 2) is a set of cryptographic hash functions designed by the
-United States National Security Agency (NSA) and first published in 2001.[3][4]`;
+United States National Security Agency (NSA) and first published in 2001.[3][4]
+They are built using the Merkle–Damgård construction, from a one-way compression function itself
+built using the Davies–Meyer structure from a specialized block cipher.`;
+}
+function stringLength(s: string) {
+  return new TextEncoder().encode(s).length;
 }
