61f27884bddd889b312c82f9201ac946ffac8806
Gregor <gregor.mitscha-baude@gmx.at>
2024-10-11 13:52:35 +0200

    more test

--- src/attestations/dynamic-sha256.test.ts
+++ src/attestations/dynamic-sha256.test.ts
@@ -1,11 +1,12 @@
-import { Bytes, Gadgets, UInt32, UInt8 } from 'o1js';
+import { Bytes, Gadgets, Provable, UInt32, UInt8 } from 'o1js';
 import { DynamicArray } from './dynamic-array.ts';
 import * as nodeAssert from 'node:assert';
 import { DynamicSHA256 } from './dynamic-sha256.ts';
+import { zip } from '../util.ts';
 
 const { SHA256 } = Gadgets;
 
-class DynamicBytes extends DynamicArray(UInt8, { maxLength: 500 }) {
+class DynamicBytes extends DynamicArray(UInt8, { maxLength: 180 }) {
   static fromString(s: string) {
     return DynamicBytes.from(
       [...new TextEncoder().encode(s)].map((t) => UInt8.from(t))
@@ -14,16 +15,56 @@ class DynamicBytes extends DynamicArray(UInt8, { maxLength: 500 }) {
 }
 
 let bytes = DynamicBytes.fromString(longString());
-let staticBytes = Bytes.fromString(longString());
+
+const StaticBytes = Bytes(longString().length);
+let staticBytes = StaticBytes.fromString(longString());
 
 nodeAssert.deepStrictEqual(
   DynamicSHA256.padding(bytes).toValue().map(blockToHexBytes),
   SHA256.padding(staticBytes).map(blockToHexBytes)
 );
-nodeAssert.deepStrictEqual(
-  DynamicSHA256.hash(bytes).toBytes(),
-  SHA256.hash(staticBytes).toBytes()
+
+let actualHash = DynamicSHA256.hash(bytes);
+let expectedHash = SHA256.hash(staticBytes);
+nodeAssert.deepStrictEqual(actualHash.toBytes(), expectedHash.toBytes());
+
+// in-circuit test
+
+async function circuit() {
+  let bytesVar = Provable.witness(DynamicBytes, () => bytes);
+  let hash = DynamicSHA256.hash(bytesVar);
+
+  zip(hash.bytes, expectedHash.bytes).forEach(([a, b], i) => {
+    a.assertEquals(b, `hash[${i}]`);
+  });
+}
+
+async function circuitStatic() {
+  let bytesVar = Provable.witness(StaticBytes, () => staticBytes);
+  let hash = SHA256.hash(bytesVar);
+
+  zip(hash.bytes, expectedHash.bytes).forEach(([a, b], i) => {
+    a.assertEquals(b, `hash[${i}]`);
+  });
+}
+
+await Provable.runAndCheck(circuit);
+await Provable.runAndCheck(circuitStatic);
+
+// constraints
+
+let constraints = await Provable.constraintSystem(circuit);
+console.log('dynamic', constraints.summary());
+
+let constraintStatic = await Provable.constraintSystem(circuitStatic);
+console.log('static', constraintStatic.summary());
+
+let ratio = constraints.rows / constraintStatic.rows;
+console.log(
+  `constraint overhead for dynamic: ${((ratio - 1) * 100).toFixed(2)}%`
 );
+
+// helpers
 
 function toHexBytes(uint32: bigint | UInt32) {
   return UInt32.from(uint32).toBigint().toString(16).padStart(8, '0');
@@ -32,10 +73,8 @@ function blockToHexBytes(block: (bigint | UInt32)[]) {
   return block.map(toHexBytes);
 }
 
+// 170 characters, needs 3 blocks
 function longString(): string {
-  return `
-Symbol.iterator
-
-The Symbol.iterator static data property represents the well-known symbol Symbol.iterator. The iterable protocol looks up this symbol for the method that returns the iterator for an object. In order for an object to be iterable, it must have an [Symbol.iterator] key.  
-`;
+  return `SHA-2 (Secure Hash Algorithm 2) is a set of cryptographic hash functions designed by the
+United States National Security Agency (NSA) and first published in 2001.[3][4]`;
 }
