c532729647ca3be06fed6ede4e3c5eb05d79e7f7
Gregor <gregor.mitscha-baude@gmx.at>
2024-10-25 15:22:41 +0200

    make example check one of several nations

--- examples/unique-hash.eg.ts
+++ examples/unique-hash.eg.ts
@@ -10,18 +10,17 @@ import {
 } from '../src/index.ts';
 import { issuerKey, owner, ownerKey } from '../tests/test-utils.ts';
 import { validateCredential } from '../src/credential-index.ts';
+import { array } from '../src/o1js-missing.ts';
 
 // example schema of the credential, which has enough entropy to be hashed into a unique id
 const Bytes32 = Bytes(32);
 const Bytes16 = Bytes(16); // 16 bytes = 128 bits = enough entropy
 
-const Data = {
-  nationality: Bytes32,
-  id: Bytes16,
-};
+const Data = { nationality: Bytes32, id: Bytes16 };
 
 // ---------------------------------------------
 // ISSUER: issue a signed credential to the owner
+
 let data = {
   nationality: Bytes32.fromString('United States of America'),
   id: Bytes16.random(),
@@ -33,6 +32,7 @@ console.log('✅ ISSUER: issued credential:', credentialJson);
 
 // ---------------------------------------------
 // WALLET: deserialize, validate and store the credential
+
 let storedCredential = Credential.fromJSON(credentialJson);
 
 await validateCredential(storedCredential);
@@ -41,18 +41,19 @@ console.log('✅ WALLET: imported and validated credential');
 
 // ---------------------------------------------
 // VERIFIER: request a presentation
+
 const spec = Spec(
   {
     signedData: Credential.Simple(Data), // schema needed here!
-    targetNationality: Claim(Bytes32),
+    targetNationalities: Claim(array(Bytes32, 3)), // TODO would make more sense as dynamic array
     appId: Claim(Bytes32),
   },
-  ({ signedData, targetNationality, appId }) => ({
+  ({ signedData, targetNationalities, appId }) => ({
     // we assert that the owner has the target nationality
     // TODO: add a one-of-many operation to make this more interesting
-    assert: Operation.equals(
+    assert: Operation.equalsOneOf(
       Operation.property(signedData, 'nationality'),
-      targetNationality
+      targetNationalities
     ),
     // we expose a unique hash of the credential data, as nullifier
     data: Operation.record({
@@ -61,15 +62,19 @@ const spec = Spec(
   })
 );
 
+const targetNationalities = ['United States of America', 'Canada', 'Mexico'];
+
 let request = PresentationRequest.noContext(spec, {
-  targetNationality: Bytes32.fromString('United States of America'),
-  appId: Bytes32.fromString('my-app-id'),
+  targetNationalities: targetNationalities.map((s) => Bytes32.fromString(s)),
+  appId: Bytes32.fromString('my-app-id:123'),
 });
 let requestJson = PresentationRequest.toJSON(request);
+
 console.log('✅ VERIFIER: created presentation request:', requestJson);
 
 // ---------------------------------------------
 // WALLET: deserialize request and create presentation
+
 console.time('compile');
 let deserialized = PresentationRequest.fromJSON<typeof request>(requestJson);
 let compiled = await Presentation.compile(deserialized);
@@ -82,10 +87,12 @@ let presentation = await Presentation.create(ownerKey, {
 });
 console.timeEnd('create');
 // TODO: to send the presentation back we need to serialize it as well
+
 console.log('✅ WALLET: created presentation:', presentation);
 
 // ---------------------------------------------
 // VERIFIER: verify the presentation, and check that the nullifier was not used yet
+
 let existingNullifiers = new Set([0x13c43f30n, 0x370f3473n, 0xe1fe0cdan]);
 
 // TODO: claims and other I/O values should be plain JS types
