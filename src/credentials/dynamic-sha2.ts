63f220ed6537beb5a26bf9db1056129af072e451
Gregor <gregor.mitscha-baude@gmx.at>
2024-11-20 15:25:24 +0100

    update mode for dynamic sha2

--- src/credentials/dynamic-sha2.ts
+++ src/credentials/dynamic-sha2.ts
@@ -1,11 +1,22 @@
-import { Bytes, Provable, UInt32, UInt64, UInt8 } from 'o1js';
+import {
+  Bytes,
+  Field,
+  Provable,
+  type ProvablePure,
+  Struct,
+  UInt32,
+  UInt64,
+  UInt8,
+} from 'o1js';
 import { DynamicArray } from './dynamic-array.ts';
 import { StaticArray } from './static-array.ts';
-import { chunk, pad } from '../util.ts';
+import { assert, chunk, pad } from '../util.ts';
 import { SHA2 } from './sha2.ts';
 import { uint64FromBytesBE, uint64ToBytesBE } from './gadgets.ts';
+import { hashSafe } from './dynamic-hash.ts';
+import { ProvableType, toFieldsPacked } from '../o1js-missing.ts';
 
-export { DynamicSHA2 };
+export { DynamicSHA2, Sha2IterationState, Sha2Iteration, Sha2FinalIteration };
 
 const DynamicSHA2 = {
   /**
@@ -29,6 +40,10 @@ const DynamicSHA2 = {
    */
   hash: sha2,
 
+  split,
+  update,
+  finalize,
+
   padding256,
   padding512,
 };
@@ -39,13 +54,15 @@ function sha2(len: 224 | 256 | 384 | 512, bytes: DynamicArray<UInt8>): Bytes {
   throw new Error('unsupported hash length');
 }
 
+type Length = 224 | 256 | 384 | 512;
+
 // static array types for blocks / state / result
 class UInt8x4 extends StaticArray(UInt8, 4) {}
 class UInt8x8 extends StaticArray(UInt8, 8) {}
 class UInt8x64 extends StaticArray(UInt8, 64) {}
 class UInt8x128 extends StaticArray(UInt8, 128) {}
-class Block extends StaticArray(UInt32, 16) {}
-class State extends StaticArray(UInt32, 8) {}
+class Block32 extends StaticArray(UInt32, 16) {}
+class State32 extends StaticArray(UInt32, 8) {}
 class Block64 extends StaticArray(UInt64, 16) {}
 class State64 extends StaticArray(UInt64, 8) {}
 const Bytes28 = Bytes(28);
@@ -58,11 +75,11 @@ function hash256(len: 224 | 256, bytes: DynamicArray<UInt8>): Bytes {
 
   // hash a dynamic number of blocks using DynamicArray.reduce()
   let state = blocks.reduce(
-    State,
-    State.from(SHA2.initialState256(len)),
+    State32,
+    State32.from(SHA2.initialState256(len)),
     (state, block) => {
       let W = SHA2.messageSchedule256(block.array);
-      return State.from(SHA2.compression256(state.array, W));
+      return State32.from(SHA2.compression256(state.array, W));
     }
   );
 
@@ -94,7 +111,7 @@ function hash512(len: 384 | 512, bytes: DynamicArray<UInt8>): Bytes {
  */
 function padding256(
   message: DynamicArray<UInt8>
-): DynamicArray<StaticArray<UInt32>> {
+): DynamicArray<StaticArray<UInt32>, bigint[]> {
   /* padded message looks like this:
   
   M ... M 0x80 0x0 ... 0x0 L L L L L L L L
@@ -132,7 +149,7 @@ function padding256(
   let blocksOfBytes = new BlocksOfBytes(chunked, numberOfBlocks);
 
   // pack each block of 64 bytes into 16 uint32s (4 bytes each)
-  let blocks = blocksOfBytes.map(Block, (block) =>
+  let blocks = blocksOfBytes.map(Block32, (block) =>
     block.chunk(4).map(UInt32, (b) => UInt32.fromBytesBE(b.array))
   );
 
@@ -207,7 +224,7 @@ function padding512(
   let chunked = chunk(padded, 128).map(UInt8x128.from);
   let blocksOfBytes = new BlocksOfBytes(chunked, numberOfBlocks);
 
-  // pack each block of 64 bytes into 16 uint64s (8 bytes each)
+  // pack each block of 128 bytes into 16 uint64s (8 bytes each)
   let blocks = blocksOfBytes.map(Block64, (block) =>
     block.chunk(8).map(UInt64, (b) => uint64FromBytesBE(b.array))
   );
@@ -239,4 +256,200 @@ function splitMultiIndex64(index: UInt32) {
   let { rest: l0, quotient: l1 } = index.divMod(128);
   let { rest: l00, quotient: l01 } = l0.divMod(8);
   return [l00.value, l01.value, l1.value] as const;
+}
+
+// updating API
+
+type Sha2IterationState<L extends Length = Length> = L extends 224 | 256
+  ? { len: L; state: State32; commitment: Field }
+  : { len: L; state: State64; commitment: Field };
+
+type Sha2Iteration =
+  | { type: 256; blocks: StaticArray<Block32> }
+  | { type: 512; blocks: StaticArray<Block64> };
+
+type Sha2FinalIteration =
+  | { type: 256; blocks: DynamicArray<Block32> }
+  | { type: 512; blocks: DynamicArray<Block64> };
+
+function initialState<L extends Length>(len: L): Sha2IterationState<L>;
+function initialState(len: Length): Sha2IterationState {
+  if (len === 224 || len === 256) {
+    return {
+      len,
+      state: State32.from(SHA2.initialState256(len)),
+      commitment: Field(0),
+    };
+  } else {
+    return {
+      len,
+      state: State64.from(SHA2.initialState512(len)),
+      commitment: Field(0),
+    };
+  }
+}
+
+function split<L extends Length>(
+  len: L,
+  blocksPerIteration: number,
+  bytes: DynamicArray<UInt8>
+): {
+  initial: Sha2IterationState<L>;
+  iterations: Sha2Iteration[];
+  final: Sha2FinalIteration;
+} {
+  let initial = initialState(len);
+
+  if (len === 224 || len === 256) {
+    let blocks = padding256(bytes);
+    let [iterations, final] = blocks.chunk(blocksPerIteration);
+    return {
+      initial,
+      iterations: iterations.array
+        .slice(0, Number(iterations.length))
+        .map((blocks) => ({ type: 256, blocks })),
+      final: { type: 256, blocks: final },
+    };
+  } else {
+    let blocks = padding512(bytes);
+    let [iterations, final] = blocks.chunk(blocksPerIteration);
+    return {
+      initial,
+      iterations: iterations.array
+        .slice(0, Number(iterations.length))
+        .map((blocks) => ({ type: 512, blocks })),
+      final: { type: 512, blocks: final },
+    };
+  }
+}
+
+function update<L extends Length>(
+  iterState: Sha2IterationState<L>,
+  iteration: Sha2Iteration
+): Sha2IterationState<L>;
+
+function update(
+  iterState: Sha2IterationState,
+  iteration: Sha2Iteration
+): Sha2IterationState {
+  if (iterState.len === 224 || iterState.len === 256) {
+    assert(iteration.type === 256, 'incompatible types');
+
+    // update hash state and commitment
+    let { state, commitment } = iterState;
+    state = iteration.blocks.reduce(state, processBlock256);
+    commitment = iteration.blocks.reduce(commitment, commitBlock256);
+
+    return { len: iterState.len, state, commitment };
+  } else {
+    assert(iterState.len === 384 || iterState.len === 512, 'invalid state');
+    assert(iteration.type === 512, 'incompatible types');
+
+    // update hash state and commitment
+    let { state, commitment } = iterState;
+    state = iteration.blocks.reduce(state, processBlock512);
+    commitment = iteration.blocks.reduce(commitment, commitBlock512);
+
+    return { len: iterState.len, state, commitment };
+  }
+}
+
+function finalize(
+  iterState: Sha2IterationState,
+  final: Sha2FinalIteration,
+  bytes: DynamicArray<UInt8>
+): Bytes {
+  if (iterState.len === 224 || iterState.len === 256) {
+    assert(final.type === 256, 'incompatible types');
+
+    // update hash state and commitment
+    let { state, commitment } = iterState;
+    state = final.blocks.reduce(State32, state, processBlock256);
+    commitment = final.blocks.reduce(Field, commitment, commitBlock256);
+
+    // recompute commitment from scratch to confirm we really hashed the input bytes
+    let expected = padding256(bytes).reduce(Field, Field(0), commitBlock256);
+    commitment.assertEquals(expected, 'invalid commitment');
+
+    // finalize hash
+    let result = state.array.flatMap((x) => x.toBytesBE());
+    return iterState.len === 224 ? Bytes28.from(result) : Bytes32.from(result);
+  } else {
+    assert(iterState.len === 384 || iterState.len === 512, 'invalid state');
+    assert(final.type === 512, 'incompatible types');
+
+    // update hash state and commitment
+    let { state, commitment } = iterState;
+    state = final.blocks.reduce(State64, state, processBlock512);
+    commitment = final.blocks.reduce(Field, commitment, commitBlock512);
+
+    // recompute commitment from scratch to confirm we really hashed the input bytes
+    let expected = padding512(bytes).reduce(Field, Field(0), commitBlock512);
+    commitment.assertEquals(expected, 'invalid commitment');
+
+    // finalize hash
+    let result = state.array.flatMap((x) => uint64ToBytesBE(x));
+    return iterState.len === 384 ? Bytes48.from(result) : Bytes64.from(result);
+  }
+}
+
+// provable types for update API
+
+function Sha2IterationState<L extends Length>(
+  len: L
+): Struct<Sha2IterationState<L>> & ProvablePure<Sha2IterationState<L>> {
+  return Struct({
+    len: ProvableType.constant(len),
+    state: State(len),
+    commitment: Field,
+  });
+}
+Sha2IterationState.initial = initialState;
+
+function Sha2Iteration(
+  len: Length,
+  blocksPerIteration: number
+): Struct<Sha2Iteration> {
+  return Struct({
+    type: ProvableType.constant(len === 224 || len === 256 ? 256 : 512),
+    blocks: StaticArray(Block(len), blocksPerIteration),
+  });
+}
+
+function Sha2FinalIteration(
+  len: Length,
+  blocksPerIteration: number
+): Struct<Sha2FinalIteration> {
+  return Struct({
+    type: ProvableType.constant(len),
+    blocks: DynamicArray(Block(len), { maxLength: blocksPerIteration }),
+  });
+}
+
+// helpers for update API
+
+function processBlock256(state: State32, block: Block32): State32 {
+  let W = SHA2.messageSchedule256(block.array);
+  return State32.from(SHA2.compression256(state.array, W));
+}
+function processBlock512(state: State64, block: Block64): State64 {
+  let W = SHA2.messageSchedule512(block.array);
+  return State64.from(SHA2.compression512(state.array, W));
+}
+
+// poseidon hash for keeping a commitment to the blocks that were hashed
+function commitBlock256(commitment: Field, block: Block32): Field {
+  let blockHash = hashSafe(toFieldsPacked(Block32, block));
+  return hashSafe([commitment, blockHash]);
+}
+function commitBlock512(commitment: Field, block: Block64): Field {
+  let blockHash = hashSafe(toFieldsPacked(Block64, block));
+  return hashSafe([commitment, blockHash]);
+}
+
+function Block(len: 224 | 256 | 384 | 512) {
+  return len === 224 || len === 256 ? Block32 : Block64;
+}
+function State(len: 224 | 256 | 384 | 512) {
+  return len === 224 || len === 256 ? State32 : State64;
 }
