52ee581a6796c7d96bfe2ab7daea298b6226fdc6
Gregor <gregor.mitscha-baude@gmx.at>
2024-12-03 22:58:10 +0100

    move test email

--- src/email/dkim.test.ts
+++ src/email/dkim.test.ts
@@ -1,5 +1,8 @@
 import { readFile } from 'fs/promises';
 import { verifyDkim } from './dkim.ts';
 
-let email = await readFile(`${import.meta.dirname}/email-good.eml`, 'utf-8');
+let email = await readFile(
+  `${import.meta.dirname}/test-emails/email-good.eml`,
+  'utf-8'
+);
 await verifyDkim(email);
