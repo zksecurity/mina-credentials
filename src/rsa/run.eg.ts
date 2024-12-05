f4b1b7a7d38bfc913693f186a9f757f102364d85
Gregor <gregor.mitscha-baude@gmx.at>
2024-12-05 11:12:05 +0100

    minor

--- src/rsa/run.eg.ts
+++ src/rsa/run.eg.ts
@@ -30,8 +30,4 @@ console.log((await rsaProgram.analyzeMethods()).run.summary());
 await rsaProgram.compile();
 
 // Run the program
-await rsaProgram.run(
-  message,
-  Bigint2048.from(signature),
-  Bigint2048.from(keys.n)
-);
+await rsaProgram.run(message, signature, keys.n);
