c4fd9e36261533522a1c0766c16f16442bcf7486
Gregor <gregor.mitscha-baude@gmx.at>
2024-12-05 12:13:08 +0100

    working o1js dkim verification

--- src/email/zkemail.ts
+++ src/email/zkemail.ts
@@ -1,11 +1,12 @@
 import { readFile } from 'fs/promises';
 import { DynamicString } from '../dynamic.ts';
-import { Bigint2048 } from '../rsa/rsa.ts';
+import { Bigint2048, rsaVerify65537 } from '../rsa/rsa.ts';
 import { fetchPublicKeyFromDNS, prepareEmailForVerification } from './dkim.ts';
 import { assert } from '../util.ts';
 import { parseRSASubjectPublicKeyInfo } from './der-parse.ts';
 import { fromBase64 } from './base64.ts';
 import { bytesToBigintBE } from '../rsa/utils.ts';
+import { Bytes } from 'o1js';
 
 type ProvableEmail = {
   /**
@@ -33,23 +34,42 @@ let email = await readFile(
   `${import.meta.dirname}/test-emails/email-good.eml`,
   'utf-8'
 );
-let provableEmail = await prepareProvableEmail(email);
-console.log(provableEmail);
+let { body, header, publicKey, signature } = await prepareProvableEmail(email);
+
+// provable types with max lengths
+let Body = DynamicString({ maxLength: 700 });
+let Header = DynamicString({ maxLength: 500 });
+
+// compute and compare the body hash
+let bodyHash = Body.from(body).hashToBytes('sha2-256');
+let bodyHashBase64 = bodyHash.base64Encode();
+
+// TODO: show that body hash is contained at the correct position from header,
+// using a secure string matching circuit
+// (might be helpful to use the dkim header as hint since it is fairly strictly formatted,
+// and known to come last in the header, and then reassemble with the other headers)
+
+// hash the header
+let headerHash = Header.from(header).hashToBytes('sha2-256');
+
+// verify the signature
+rsaVerify65537(headerHash, signature, publicKey);
 
 async function prepareProvableEmail(email: string): Promise<ProvableEmail> {
   let { canonicalBody, canonicalHeader, dkimHeader } =
     prepareEmailForVerification(email);
+  assert(dkimHeader.hashAlgo === 'sha256', 'must use sha256 hash');
+  assert(dkimHeader.signAlgo === 'rsa', 'must use rsa signature');
 
   let { publicKeyBytesDer, modulusLength } = await fetchPublicKeyFromDNS(
     dkimHeader
   );
-  assert(modulusLength === 2048, 'Invalid modulus length');
+  assert(modulusLength === 2048, 'modulus length must be 2048');
 
   // parse DER-encoded `SubjectPublicKeyInfo`
   let { n, e } = parseRSASubjectPublicKeyInfo(publicKeyBytesDer);
 
-  // public exponent MUST be 65537
-  assert(e === 65537n, 'public exponent is 65537');
+  assert(e === 65537n, 'public exponent must be 65537');
   let publicKey = Bigint2048.from(n);
 
   // signature encoding:
