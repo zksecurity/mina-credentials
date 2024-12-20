30e2a3de40a7f3aee6a98a682ffb138ce27e29e2
Gregor <gregor.mitscha-baude@gmx.at>
2024-12-20 14:22:27 +0100

    minor

--- examples/web-demo/api-server/server.ts
+++ examples/web-demo/api-server/server.ts
@@ -16,7 +16,7 @@ async function readBody(req: http.IncomingMessage): Promise<string> {
 
 // CORS headers
 const corsHeaders = {
-  'Access-Control-Allow-Origin': 'http://localhost:5173',
+  'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type',
 };
