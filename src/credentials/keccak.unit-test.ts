22af7f299ef2b7b49a0e447d5a8046bee93d3acb
Gregor <gregor.mitscha-baude@gmx.at>
2025-01-31 21:51:49 +0100

    add test on long message

--- src/credentials/keccak.unit-test.ts
+++ src/credentials/keccak.unit-test.ts
@@ -2,9 +2,18 @@ import { Bytes, Keccak } from 'o1js';
 import { keccak256 } from './keccak-dynamic.ts';
 import { assert } from '../util.ts';
 
-let message = Bytes(32).fromString('hello world');
+let shortMessage = 'hello world';
+let longMessage =
+  'SHA-3 (Secure Hash Algorithm 3) is the latest[4] member of the Secure Hash Algorithm family of standards, released by NIST on August 5, 2015.[5][6][7] Although part of the same series of standards, SHA-3 is internally different from the MD5-like structure of SHA-1 and SHA-2. ';
 
-let hash1 = Keccak.ethereum(message);
-let hash2 = keccak256(message);
+assert(
+  Keccak.ethereum(Bytes.fromString(shortMessage)).toHex() ===
+    keccak256(Bytes.fromString(shortMessage)).toHex(),
+  'hashes are not equal (short message)'
+);
 
-assert(hash1.toHex() === hash2.toHex(), 'hashes are not equal');
+assert(
+  Keccak.ethereum(Bytes.fromString(longMessage)).toHex() ===
+    keccak256(Bytes.fromString(longMessage)).toHex(),
+  'hashes are not equal (long message)'
+);
