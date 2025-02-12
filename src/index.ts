dca0d21448fcb0d08732863907d6cab14ea716c9
Gregor <gregor.mitscha-baude@gmx.at>
2025-02-12 09:19:48 +0100

    don't export non-core stuff

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,6 @@ export { StaticArray } from './dynamic/static-array.ts';
 export { DynamicBytes } from './dynamic/dynamic-bytes.ts';
 export { DynamicString } from './dynamic/dynamic-string.ts';
 export { DynamicRecord } from './dynamic/dynamic-record.ts';
-export { DynamicSHA2 } from './dynamic/dynamic-sha2.ts';
 export {
   hashDynamic,
   hashDynamicWithPrefix,
